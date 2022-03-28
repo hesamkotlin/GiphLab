0a55c9447efc1ed665c79d1223003e3c0509a8c3
hesamkotlin <hesam.naderi.kotlin@gmail.com>
2022-03-28 13:30:31 +0430

    add: Hilt annotation to classes related to ui

--- app/src/main/java/com/example/giphlab/ui/MainActivity.kt
+++ app/src/main/java/com/example/giphlab/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.giphlab.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.giphlab.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
